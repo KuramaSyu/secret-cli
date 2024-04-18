7f1fb784809248159e060a3185054ee30f1f2816
kurama <52566613+zp33dy@users.noreply.github.com>
2024-04-18 15:33:00 +0200

    changed length arg to u32

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use rand::distributions::Alphanumeric;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = "32")]
-    length: String
+    #[arg(short, long, default_value_t = 32)]
+    length: u32
 }
 
 fn main() {
     let args = Args::parse();
-    let number: i32 = args.length.parse().expect("length needs to be a number");
+    let number: u32 = args.length;
     let mut rng = rand::thread_rng();
     let mut secret: String = String::from("");
     for _ in 0..number {
