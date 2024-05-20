404de489a6930ed6d38b1807bce2884d19422200
kurama <52566613+zp33dy@users.noreply.github.com>
2024-05-20 23:14:29 +0200

    added --word

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use clap::{Parser, ArgAction}; //Subcommand
 use rand::prelude::*;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::HashMap;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = "A tool to generate secrets. By default it uses numbers, lower case letters and upper case letters (-naA)")]
 struct Args {
-    #[arg(default_value_t = 32)]
-    length: u32,
-    #[arg(short = 'n', long, action = ArgAction::SetTrue)]
+    #[arg(default_value = None)]
+    length: Option<u32>,
+    #[arg(short = 'n', long, action = ArgAction::SetTrue, help = "Wether to use numbers")]
     numbers: bool,
-    #[arg(short = 'v', long, action = ArgAction::SetTrue)]
+    #[arg(short = 'v', long, action = ArgAction::SetTrue, help = "Wether to be verbose")]
     verbose: bool,
-    #[arg(short = 'a', long, action = ArgAction::SetTrue)]
+    #[arg(short = 'a', long, action = ArgAction::SetTrue, help = "Wether to use lower case letters")]
     lower_letters: bool,
-    #[arg(short = 'A', long, action = ArgAction::SetTrue)]
+    #[arg(short = 'A', long, action = ArgAction::SetTrue, help = "Wether to use upper case letters")]
     upper_letters: bool,
-    #[arg(short = 's', long, action = ArgAction::SetTrue)]
-    symbols: bool
+    #[arg(short = 's', long, action = ArgAction::SetTrue, help = "Wether to use symbols")]
+    symbols: bool,
+    #[arg(short = 'w', long, action = ArgAction::SetTrue, help = "Wether to use words instead of characters")]
+    words: bool
 }
 
 fn main() {
     let args = Args::parse();
-    let length: u32 = args.length;
+    let length: Option<u32> = args.length;
     let verbose: bool = args.verbose;
-    let mut secret: String = String::from("");
-    let mut rng = rand::thread_rng();
     let character_set: String;
+    let secret: String;
     if 
         !args.numbers 
         && !args.lower_letters 
@@ -45,11 +50,13 @@ fn main() {
             args.symbols
         );
     }
-
-    for _ in 0..length{
-        let index = rng.gen_range(0..character_set.len());
-        secret.push_str(
-            &character_set.chars().nth(index).unwrap().to_string()
+    if args.words {
+        let wordlist = load_file("./wordlists/eng.txt").unwrap();
+        secret = generate_word_secret(wordlist, args.length.unwrap_or(5) as i32, args.numbers);
+    } else {
+        secret = generate_character_secret(
+            character_set, 
+            length.unwrap_or(20) as i32
         );
     }
     if verbose {
@@ -82,4 +89,55 @@ fn make_character_set(
         character_set.push_str("!@#$%&*_+-=.?");
     }
     return character_set
+}
+
+fn generate_character_secret(character_set: String, length: i32) -> String {
+    let mut secret: String = String::from("");
+    let mut rng = rand::thread_rng();
+    for _ in 0..length{
+        let index = rng.gen_range(0..character_set.len());
+        secret.push_str(
+            &character_set.chars().nth(index).unwrap().to_string()
+        );
+    }
+    secret
+}
+
+fn load_file(filename: &str) -> io::Result<Vec<String>> {
+    let file = File::open(
+        &Path::new(filename)
+    )?;
+    let reader = io::BufReader::new(file);
+    let mut words = Vec::new();
+    for line in reader.lines() {
+        words.push(line?);
+    }
+    Ok(words)
+}
+
+fn generate_word_secret(words: Vec<String>, length: i32, with_numbers: bool) -> String {
+    let mut secret = String::new();
+    let mut rng = rand::thread_rng();
+    for n_ in 0..length {
+        let index = rng.gen_range(0..words.len());
+        let word = &words[index];
+        if n_ > 0 {
+            secret.push_str(&format!("-{word}"));
+        } else {
+            secret.push_str(word);
+        }
+    }
+    let leetspeak: HashMap<&str, &str> = [
+        ("a", "4"),
+        ("e", "3"),
+        ("o", "0"),
+        ("i", "1"),
+    ].iter().cloned().collect();
+    if !with_numbers {
+        return secret
+    }
+    for (key, value) in leetspeak {
+        secret = secret.replace(key, value);
+    }
+    secret
 }
