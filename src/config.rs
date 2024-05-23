adac06717c38d75c8a7d52616f4d1134a035d60d
kurama <52566613+zp33dy@users.noreply.github.com>
2024-05-23 22:02:24 +0200

    added functions to load yaml config

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,21 @@
-extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
+extern crate yaml_rust2;
+use yaml_rust2::{YamlLoader, YamlEmitter};
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
 
-fn load_config(path: &str) -> Result<String> {
-    let handler: String = File::open(path)?;
+pub fn load_config(path: &str) -> Vec<yaml_rust2::Yaml> {
+    let mut handler: File = File::open(path).unwrap();
     let mut content = String::new();
-    handler.read_to_string(&mut content)?;
-    return Ok(content)
+    handler.read_to_string(&mut content).unwrap();
+    let yaml: Vec<yaml_rust2::Yaml> = YamlLoader::load_from_str(&content).unwrap();
+    return yaml
 }
+
+pub fn get_language(config: &Vec<yaml_rust2::Yaml>) -> Option<&str> {
+    config[0]["options"]["language"].as_str()
+}
+
+
+
 
