4ae3d80d5d7972022f33ce243e7cbe59cd8e63de
kurama <52566613+zp33dy@users.noreply.github.com>
2024-05-25 19:36:16 +0200

    implemented functions with serde instead of yaml2

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,46 @@
-extern crate yaml_rust2;
-use yaml_rust2::{YamlLoader, YamlEmitter};
-use std::fs::File;
+use serde_yaml;
+use serde::{Serialize, Deserialize};
+use std::borrow::BorrowMut;
+use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io;
+use std::collections::BTreeMap;
 
-pub fn load_config(path: &str) -> Vec<yaml_rust2::Yaml> {
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Options {
+    language: String
+}
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Config {
+    options: Options
+}
+
+pub fn load_config(path: &str) -> Config {
     let mut handler: File = File::open(path).unwrap();
     let mut content = String::new();
     handler.read_to_string(&mut content).unwrap();
-    let yaml: Vec<yaml_rust2::Yaml> = YamlLoader::load_from_str(&content).unwrap();
+    let yaml: Config = serde_yaml::from_str(content.as_str()).unwrap();
     return yaml
 }
 
-pub fn get_language(config: &Vec<yaml_rust2::Yaml>) -> Option<&str> {
-    config[0]["options"]["language"].as_str()
+pub fn get_language(config: &mut Config) -> Option<&str> {
+    Some(&config.options.language)
+}
+
+pub fn set_language(config: &mut Config, lang: &str, path: &str) -> () {
+    let mut handler = OpenOptions::new().write(true).open(path);
+    let is_ok = match handler {
+        Err(ref e) => {
+            println!("Info: Can't change config to {} because: {}", lang, e);
+            false
+        },
+        _ => true
+    };
+    if !is_ok {
+        return
+    }
+    config.options.language = lang.to_owned();
+    serde_yaml::to_writer(handler.unwrap(), &config).unwrap();
 }
 
 
